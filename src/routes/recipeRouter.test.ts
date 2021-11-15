b644ad6116be600f310aa0634afeaa7577751fba
michael-levitski <66583830+michael-levitski@users.noreply.github.com>
2021-11-15 12:18:03 -0800

    remove unnecessary async keywords

--- src/routes/recipeRouter.test.ts
+++ src/routes/recipeRouter.test.ts
@@ -124,7 +124,7 @@ describe("POST /recipes", () => {
       });
     });
 
-    test("returns JSON body with error message and 400 status code", async () => {
+    test("returns JSON body with error message and 400 status code", () => {
       expect(response.headers["content-type"].includes("json")).toBe(true);
       expect(response.statusCode).toBe(400);
       expect(response.body).toEqual({
@@ -195,7 +195,7 @@ describe("PUT /recipes", () => {
       });
     });
 
-    test("returns JSON body with error message and 404 status code", async () => {
+    test("returns JSON body with error message and 404 status code", () => {
       expect(response.headers["content-type"].includes("json")).toBe(true);
       expect(response.statusCode).toBe(404);
       expect(response.body).toEqual({
