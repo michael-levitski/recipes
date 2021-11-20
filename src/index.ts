e5d8633e55a7bcaf988a9f3eec763e59e4f100bb
michael-levitski <levitski.michael@gmail.com>
2021-11-19 18:36:00 -0800

    create env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import "./env";
 import App from "./app";
 import * as inMemoryRepository from "./repositories/inMemoryRecipeRepo";
 import { Env } from "./types";
+import "./repositories/sequelizeRepo"
 
 const { PORT } = process.env as Env;
 const app = App({repository: inMemoryRepository});
