c51343a944aa0961cb0422ece01173541bcedbaa
michael-levitski <levitski.michael@gmail.com>
2021-11-23 18:23:40 -0800

    make router functions async

--- src/routes/recipeRouter.ts
+++ src/routes/recipeRouter.ts
@@ -26,23 +26,23 @@ function validateBody(req: Request, res: Response) {
 }
 
 export default function RecipeRouter(recipeRepository: RecipeRepository) {
-  router.get("/", cacheRecipeNames, function (_, res) {
-    const recipeNames = recipeRepository.getRecipeNames();
+  router.get("/", cacheRecipeNames, async function (_, res) {
+    const recipeNames = await recipeRepository.getRecipeNames();
     send.ok<RecipeNames>(res, { recipeNames });
   });
 
-  router.get("/details/:recipeName", function (req, res) {
+  router.get("/details/:recipeName", async function (req, res) {
     const { recipeName } = req.params;
-    const recipeDetails = recipeRepository.getRecipeDetails(recipeName);
+    const recipeDetails = await recipeRepository.getRecipeDetails(recipeName);
     send.ok<RecipeDetails | EmptyObject>(res, recipeDetails);
   });
 
-  router.post("/", checkBody, function (req: Request, res: Response) {
+  router.post("/", checkBody, async function (req: Request, res: Response) {
     const sent = validateBody(req, res);
     if (sent) return;
 
     const recipe = req.body;
-    const success = recipeRepository.insertRecipe(recipe);
+    const success = await recipeRepository.insertRecipe(recipe);
     const error = "Recipe already exists";
 
     clearRecipeNamesCache(success);
@@ -50,12 +50,12 @@ export default function RecipeRouter(recipeRepository: RecipeRepository) {
     else send.badRequest(res, error);
   });
 
-  router.put("/", checkBody, function (req: Request, res: Response) {
+  router.put("/", checkBody, async function (req: Request, res: Response) {
     const sent = validateBody(req, res);
     if (sent) return;
 
     const recipe = req.body;
-    const success = recipeRepository.updateRecipe(recipe);
+    const success = await recipeRepository.updateRecipe(recipe);
     const error = "Recipe does not exist";
 
     if (success) send.noContent(res);
